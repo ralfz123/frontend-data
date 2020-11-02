acb7b4f6a743b66dc8a7293001440fb8c8dc09b8
ralfz123 <ralf_zonneveld@hotmail.com>
2020-11-02 12:53:09 +0100

    Added comments

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-console.log('Connection works');
-chargingPointsData = 'https://opendata.rdw.nl/resource/b3us-f26s.json';
+chargingPointsData = 'https://opendata.rdw.nl/resource/b3us-f26s.json'; // API endpoint - Charging Points (dataset 1)
 
 
 // Fetching data - API
 fetch(chargingPointsData)
-    .then(res => res.json())
-    .then(data => console.log("Data is:", data))
-    .catch(error => console.log('ERROR'))
+    .then(res => res.json()) // Translates to JSON format
+    .then(data => console.log("Data is:", data)) // Logging the retrieved data
+    .catch(error => console.log('ERROR')) // Catches and shows an error when it's occurring
 
 // Clean & Transform data
 //    - transform random/incorrect data in NULL
 
 // Combine datasets
 //    - Filter out which columns I do not use
+//
