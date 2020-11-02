7e100ef1c885168291072eceda1b403387e3a53a
ralfz123 <ralf_zonneveld@hotmail.com>
2020-11-02 12:40:01 +0100

    Fetching data succeeded

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-console.log('Connection works')
+console.log('Connection works');
+chargingPointsData = 'https://opendata.rdw.nl/resource/b3us-f26s.json';
+
 
 // Fetching data - API
+fetch(chargingPointsData)
+    .then(res => res.json())
+    .then(data => console.log("Data is:", data))
+    .catch(error => console.log('ERROR'))
 
 // Clean & Transform data
 //    - transform random/incorrect data in NULL
