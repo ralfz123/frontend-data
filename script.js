66efaf963625e4555d64d4cb36a610db2d91bea2
ralfz123 <ralf_zonneveld@hotmail.com>
2020-11-02 12:03:10 +0100

    Added basic files with good connection

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('Connection works')
+
+// Fetching data - API
+
+// Clean & Transform data
+//    - transform random/incorrect data in NULL
+
+// Combine datasets
+//    - Filter out which columns I do not use
