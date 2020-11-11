7110a95eca146314d695e28c08d732d4629fd51a
ralfz123 <ralf_zonneveld@hotmail.com>
2020-11-11 15:37:22 +0100

    Getting ids is solved and added a filter for day and night time

--- d3/dataplots/index.js
+++ d3/dataplots/index.js
@@ -4,41 +4,72 @@
 // Cleaning data
 // Transforming data
 
-const endpointOne = ('../data/dataDay.json'); // Data from a Day - 08:00h
-const endpointTwo = ('../data/dataEve.json'); // Data from a Eve - 20:00h
+const endpointOne = '../data/dataDay.json'; // Data from a Day - 08:00h
+const endpointTwo = '../data/dataEve.json'; // Data from a Eve - 20:00h
 
-const statusAvailable = 'available'; // Available Charging points 
-const statusCharging = 'charging'; // Busy Charging points 
-
+// const statusAvailable = 'available'; // Available Charging points
+// const statusCharging = 'charging'; // Busy Charging points
 
 // Fetching data
 // Receiving data using fetch()
 async function fetchingData(url) {
-		const response = await fetch(url); // Waits till the data is fetched
-		const data = await response.json(); // Waits till the data is formatted to JSON
-		console.log(data)
-		return data; // Return the JSON data
-	}
-	// const getData = fetchingData(endpointOne)
-	// .then (data => {
-		// console.log(data))
-	// }
-	
-	const getData = fetchingData(endpointOne)
-	console.log(getData[0])
+	const response = await fetch(url); // Waits till the data is fetched
+	const data = await response.json(); // Waits till the data is formatted to JSON
+	// console.log(data);
+	return data; // Return the JSON data
+}
 
+// async function combineData() {
+// 	const dayData = await fetchingData(endpointOne)
+// 	const nightData = await fetchingData(endpointTwo)
+
+// 	// Do something to combine the data
+// 	const combined = {dayData, nightData}
+
+// 	// Return the combinedData
+// 	return combined
+// }
+
+// filter option dataviz - On click fetch data to determine which time of day it is
+function handleClick(timeOfDay) {
+	const endpoint = timeOfDay === 'day' ? endpointOne : endpointTwo 
+
+	fetchingData(endpoint).then(res => {
+		console.log(`It's currently ${timeOfDay} time`)
+		console.log(res.data)
+	})
+}
+
+handleClick('day')
+
+// Get the unique ID's from each datapoint
+fetchingData(endpointOne).then((data) => {
+	console.log(data.data[0].id)
+	console.log(data.data[1].id)
+})
+const getData = fetchingData(endpointOne).then(console.log)
+
+// Getting ID's
+//  function findId () {
+// 	return data === "id"
+//  }
+//  console.log(getData.find(findId))
+
+// const getData = fetchingData(endpointOne)
+// .then (data => {
+// console.log(data))
+// }
 
 // Checks if the unique key is present - Sam was helping me
-	// const key = 'maximumvehicleheight';
-	// let missing = false;
+// const key = 'maximumvehicleheight';
+// let missing = false;
 
-	// for (let i of dataAll) {
-	// 	if (!i[key]) {
-	// 		missing = true;
-	// 	}
-	// }
-	// console.log(key, "is missing:", missing);
-
+// for (let i of dataAll) {
+// 	if (!i[key]) {
+// 		missing = true;
+// 	}
+// }
+// console.log(key, "is missing:", missing);
 
 // Filter and transform data - Make a new Array based on an unique key and checks if value is correct
 // function newDataArray(dataArray, key) {
